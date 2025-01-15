9529c66c87897b046a8299056844acc43a7cb685
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-01-14 19:09:07 -0500

    8

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ terraform {
             version = "~>3.0"}
         http = {
             source  = "hashicorp/http"
-            version = "~>= 2.1.0"}
+            version = ">= 2.1.0"}
         random = {
             source = "hashcorp/random"
-            version = "~>= 3.1.0"}
+            version = ">= 3.1.0"}
         local = {
             source = "hashcorp/local"
-            version = "~>=2.1.0"}
+            version = ">=2.1.0"}
         tls = {
             source  = "hashicorp/tls"
-            version = "~> 3.1.0"
+            version = "=> 3.1.0"
         }
     }
 }
