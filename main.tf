3483e505747b8e37aec0fa3302257326ef23271d
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-07-14 16:01:26 -0400

    v1_0_0_6

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "docker_host" {
-  ami                         = "ami-04b4f1a9cf54c11d0"  # Official Ubuntu Server 22.04 LTS AMI (HVM, SSD-backed) for us-east-1
+  ami                         = "ami-04b4f1a9cf54c11d0"
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  key_name                    = aws_key_pair.zero_node_key.key_name
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -12,21 +12,23 @@ resource "aws_instance" "docker_host" {
     Environment = "dev"
   }
 }
+
 resource "aws_key_pair" "zero_node_key" {
-  key_name   = "Zero_Node_Key" 
-  public_key = file("/home/ubuntu/projects/ssh.pub") 
+  key_name   = var.key_name
+  public_key = file("/home/ubuntu/projects/ssh.pub")
 }
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH access for Ansible"
-  vpc_id      = "sg-0325e0fb5c34df152"
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Or restrict to your IP for security
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
