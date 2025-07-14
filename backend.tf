1d5c9e8e9915136e46b0100b338592f9eaa39d1e
AlexandrNeverov <nev.al.vic1@gmail.com>
2025-07-14 15:36:13 -0400

    v1_0_0_4

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-zero-1752515536"
-    key            = "state/terraform.tfstate"             
-    region         = "us-east-1"
-    use_lock_table = false                                 
+    bucket = "terraform-backend-zero-1752515536"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
   }
 }
